f002d8c308f23472a285ad24e4c783a3403a668e
rockgarden <rockgarden@sina.com>
2016-04-11 23:43:57 +0800

    add Behavior tip

--- myapp/src/main/java/com/rockgarden/myapp/behavior/QuickReturnFooterBehavior.java
+++ myapp/src/main/java/com/rockgarden/myapp/behavior/QuickReturnFooterBehavior.java
@@ -10,13 +10,17 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
 import android.view.animation.Interpolator;
+import android.widget.ImageView;
 
 /**
  * CoordinatorLayout Behavior for a quick return footer
+ * 通过Behavior实显list的回滚
  *
  * When a nested ScrollView is scrolled down, the quick return view will disappear.
  * When the ScrollView is scrolled back up, the quick return view will reappear.
  *
+ * 通过app:layout_behavior注入基于CoordinatorLayout布局xml
+ * https://medium.com/@bherbst/quick-return-with-recyclerview-e70c8da9b4c1
  * @author bherbst
  */
 @SuppressWarnings("unused")
@@ -31,13 +35,54 @@ public class QuickReturnFooterBehavior extends CoordinatorLayout.Behavior<View> 
         super(context, attrs);
     }
 
+    /**
+     * 获取RecyclerView中的垂直滚动，因此只在滚动事件包括垂直信息的时候才返回true
+     * @param coordinatorLayout
+     * @param child
+     * @param directTargetChild
+     * @param target
+     * @param nestedScrollAxes
+     * @return
+     */
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
         return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
+    /**
+     * 声明CoordinatorLayoutBehavior与什么View相关
+     * 比如，让一个View基于一个ImageView的表现作什么事情
+     * @param parent
+     * @param child
+     * @param dependency
+     * @return
+     */
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof ImageView;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        // Adjust the child View accordingly
+        return true;
+    }
+
+    /**
+     * 响应滚动事件的回调方法onNestedPreScroll()
+     * PreScroll指的是一些Behaviors(比如和AppBarLayout使用的)会消费掉部分滚动事件,
+     * 如AppBarLayout允许toolbar随着内容的滚动而滚动出去,它就会消费掉任意多的滚动距离,
+     * 这需要向其他view题示已经计算了一段滚动距离了;
+     * @param coordinatorLayout
+     * @param child
+     * @param target
+     * @param dx
+     * @param dy
+     * @param consumed
+     */
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        // 方向改变的时候取消动画
         if (dy > 0 && mDySinceDirectionChange < 0
                 || dy < 0 && mDySinceDirectionChange > 0) {
             // We detected a direction change- cancel existing animations and reset our cumulative delta Y
@@ -46,7 +91,7 @@ public class QuickReturnFooterBehavior extends CoordinatorLayout.Behavior<View> 
         }
 
         mDySinceDirectionChange += dy;
-
+        // 检查了child是否可见,view可见的时才隐藏,隐藏时才显示
         if (mDySinceDirectionChange > child.getHeight()
                 && child.getVisibility() == View.VISIBLE
                 && !mIsHiding) {
@@ -72,7 +117,7 @@ public class QuickReturnFooterBehavior extends CoordinatorLayout.Behavior<View> 
                 .translationY(view.getHeight())
                 .setInterpolator(INTERPOLATOR)
                 .setDuration(200);
-
+        //为animation添加一个AnimatorListener,在animation完成的时候更新view的visibility
         animator.setListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {}
